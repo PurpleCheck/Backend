4a759ee0faf3d3b69b84ed9f2d6cc5495a56ce51
maplejh <cjh0313c@gmail.com>
2022-08-20 04:55:11 +0900

    feat(20220820): Add Controller

--- zeroError/src/main/java/purpleCheck/zeroError/controller/InspectionInfoController.java
+++ zeroError/src/main/java/purpleCheck/zeroError/controller/InspectionInfoController.java
@@ -1,4 +1,27 @@
 package purpleCheck.zeroError.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import purpleCheck.zeroError.newForm.UpdateStatusReqForm;
+import purpleCheck.zeroError.newForm.OrderListResForm;
+import purpleCheck.zeroError.newForm.UpdateStatusResForm;
+import purpleCheck.zeroError.service.InspectionInfoService;
+
+@Slf4j // 로깅
+@RestController
+@RequestMapping("/inspections/{inspectId}")
+@RequiredArgsConstructor
 public class InspectionInfoController {
+
+    private final InspectionInfoService inspectionInfoService;
+
+    @GetMapping
+    public OrderListResForm orderList(@PathVariable long inspectId){
+        return inspectionInfoService.getOrderListForm(inspectId);
+    }
+    @PatchMapping("/finish")
+    public UpdateStatusResForm updateStatus(@PathVariable long inspectId, @RequestBody UpdateStatusReqForm updateStatusReqForm) {
+        return inspectionInfoService.updateStatus(inspectId,updateStatusReqForm.isCompleteYN());
+    }
 }
