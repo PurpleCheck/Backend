84f00bda7e3d758c9619ed0d7f029c7b530580c5
maplejh <cjh0313c@gmail.com>
2022-08-20 04:57:36 +0900

    feat(20220820): Add Exception

---
+++ zeroError/src/main/java/purpleCheck/zeroError/exception/UpdateFailException.java
@@ -0,0 +1,11 @@
+package purpleCheck.zeroError.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class UpdateFailException extends RuntimeException{
+    public UpdateFailException(String message) {
+        super(message);
+    }
+}
