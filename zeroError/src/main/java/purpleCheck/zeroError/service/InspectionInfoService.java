7683a163493a6f269c2345f7d209acaee6084c2e
maplejh <cjh0313c@gmail.com>
2022-08-20 04:56:26 +0900

    feat(20220820): Add Service

--- zeroError/src/main/java/purpleCheck/zeroError/service/InspectionInfoService.java
+++ zeroError/src/main/java/purpleCheck/zeroError/service/InspectionInfoService.java
@@ -1,4 +1,60 @@
 package purpleCheck.zeroError.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import purpleCheck.zeroError.domain.entity.InspectInfo;
+import purpleCheck.zeroError.exception.NoSuchInspectIdFoundException;
+import purpleCheck.zeroError.exception.UpdateFailException;
+import purpleCheck.zeroError.newForm.OrderListResForm;
+import purpleCheck.zeroError.newForm.OrderItemDto;
+import purpleCheck.zeroError.newForm.UpdateStatusResForm;
+import purpleCheck.zeroError.repository.InspectInfoRepository;
+import purpleCheck.zeroError.repository.OrderItemRepository;
+import purpleCheck.zeroError.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class InspectionInfoService {
+
+    private final InspectInfoRepository inspectInfoRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderListResForm getOrderListForm(long id) {
+        InspectInfo inspectInfo = inspectInfoRepository.findById(id);
+
+        if (inspectInfo==null){
+            throw new NoSuchInspectIdFoundException("no such inspectId");
+        }
+
+        long orderId = inspectInfo.getOrder().getId();
+        String trackingId = inspectInfo.getTracking().getId();
+        List<OrderItemDto> orderItems = orderItemRepository.findById(orderId);
+        int totalCount = orderRepository.findById(orderId).getTotalCount();
+        return new OrderListResForm(id, orderId, trackingId, orderItems, totalCount);
+    }
+
+    @Transactional
+    public UpdateStatusResForm updateStatus(long id, boolean completeYN) {
+        InspectInfo inspectInfo = inspectInfoRepository.findById(id);
+
+        if (inspectInfo==null){
+            throw new NoSuchInspectIdFoundException("no such inspectId");
+        }
+
+        boolean isUpdate = inspectInfo.updateCompStatus(completeYN);
+
+        if (!isUpdate){
+            throw new UpdateFailException("fail to update status");
+        }
+
+        return new UpdateStatusResForm(isUpdate);
+
+    }
+
+
 }
